0489035e5a3f30dd4fcc8dd63ffac73780bb7264
itsmeadriana <itsmeadriana@gmail.com>
2024-05-13 17:06:11 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Adriana Nieves on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
